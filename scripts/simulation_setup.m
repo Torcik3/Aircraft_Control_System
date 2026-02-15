572e53748b85f660ebf16c7bf71b943d10510aaf
Torcik3 <tciryt23@gmail.com>
2026-02-15 22:18:04 +0100

    scripts

---
+++ scripts/simulation_setup.m
@@ -0,0 +1,16 @@
+
+clear; close all; clc; 
+
+
+Ix = 100;     
+Iy = 150;     
+Iz = 200;     
+
+K_delta_roll = 10;
+C_T_rol = 0.5;   
+
+K_delta_pitch = 20; 
+C_T_pitch = 0.6; 
+
+K_delta_yaw = 8;
+C_T_yaw = 0.4;
